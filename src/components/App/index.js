5eb36816b7eac99ec9756e740df938cfc765a9ed
JaaKto <33008680+JaaKto@users.noreply.github.com>
2018-05-29 23:55:29 +0200

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Main from '../Main';
+import logo from '../../assets/logo.svg';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">TV Series List</h1>
+        </header>
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './index.css';
+
+const Loader = props => (
+  <div>
+    <img src={logo} alt='loader icon'className="loader"/>
+  </div>
+)
+
+export default Loader;
