bd6578f73bbd9f860ccebdb3d23a87a8ed1faded
JaaKto <33008680+JaaKto@users.noreply.github.com>
2018-09-21 17:43:48 +0200

    set unique key for each img

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -17,19 +17,46 @@ class SingleSeries extends Component {
 
   render() {
     const { show } = this.state;
+    console.log(show)
+
+
+
     let showEpisodes = () => {
       const n = [];
       if (show !== null) {
         for(let i=0;i<10;i++){
           n.push(show._embedded.episodes[(Math.floor(Math.random() * show._embedded.episodes.length))].image.original);
         }
-        let images = n.map((i) => {
-          return <img alt='Show' src={i} />
+        let images = n.map((i, index) => {
+          return <img key={index} alt='Show' src={i} />
           // return <img style={{maxHeight: 'calc(100vh - 100px)', maxWidth: 'calc(100vw - 1500px)'}} alt='Show' src={i} />
         });
         return images;
       }
     }
+
+    // const randomNumbers = [];
+    // while(randomNumbers.length < 10){
+    //     console.log(show._embedded.episodes.length)
+    // 	let x = Math.floor(Math.random() * show._embedded.episodes.length);
+    // 	if (!randomNumbers.includes(x)) {
+    // 		randomNumbers.push(x);
+    // 	}
+    // }
+    //
+    // let showEpisodes = () => {
+    //   const n = [];
+    //   if (show !== null) {
+    //     for(let i=0;i<=randomNumbers.length;i++){
+    //       n.push(show._embedded.episodes[randomNumbers[i]].image.original);
+    //     }
+    //     let images = n.map((i) => {
+    //       return <img alt='Show' src={i} />
+    //       // return <img style={{maxHeight: 'calc(100vh - 100px)', maxWidth: 'calc(100vw - 1500px)'}} alt='Show' src={i} />
+    //     });
+    //     return images;
+    //   }
+    // }
 
     return (
       <div>
