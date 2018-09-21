e1911076bbea96e529e917d66216d9f775b52d71
JaaKto <33008680+JaaKto@users.noreply.github.com>
2018-09-21 16:47:28 +0200

    fixed random photos

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Loader from '../../components/Loader'
+import Loader from '../../components/Loader';
+import './index.css';
 
 class SingleSeries extends Component {
   state = {
@@ -11,19 +12,23 @@ class SingleSeries extends Component {
     fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
       .then(response => response.json())
       .then(json => this.setState({ show: json }));
+
   }
 
   render() {
     const { show } = this.state;
-    console.log(show);
-
-    const randomNum = (min, episodes ) => {
-    var n = [];
-    for(let i=0;i<6;i++){
-    n.push(Math.floor(Math.random() * episodes) + min);
-    return n;
-    console.log(n)
-    }
+    let showEpisodes = () => {
+      const n = [];
+      if (show !== null) {
+        for(let i=0;i<10;i++){
+          n.push(show._embedded.episodes[(Math.floor(Math.random() * show._embedded.episodes.length))].image.original);
+        }
+        let images = n.map((i) => {
+          return <img alt='Show' src={i} />
+          // return <img style={{maxHeight: 'calc(100vh - 100px)', maxWidth: 'calc(100vw - 1500px)'}} alt='Show' src={i} />
+        });
+        return images;
+      }
     }
 
     return (
@@ -32,22 +37,22 @@ class SingleSeries extends Component {
         {
           show !== null
           &&
-          <div style={{display: 'grid', gridTemplateColumns: 'auto auto auto'}} >
-            <img style={{height: 'calc(100vh - 100px)'}} alt='Show' src={show.image.original} />
-            <div>
-              <h1>{show.name}</h1>
-              <p>Premiered - {show.premiered}</p>
-              <p>Rating - {show.rating.average}</p>
-              <p>Episodes - {show._embedded.episodes.length}</p>
-              <div dangerouslySetInnerHTML={{__html: show.summary}} />
-              <h2>{Math.floor((Math.random() * show._embedded.episodes.length))}</h2>
+          <div className='main-container' style={{display: 'flex'}} >
+            <img className='poster' alt='Show' src={show.image.original} />
+            <div className='grid-container' >
+              <div className='tv-series-container'>
+                <h1>{show.name}</h1>
+                <p>Premiered - {show.premiered}</p>
+                <p>Rating - {show.rating.average}</p>
+                <p>Episodes - {show._embedded.episodes.length}</p>
+                <div dangerouslySetInnerHTML={{__html: show.summary}} />
+              </div>
+              {showEpisodes()}
             </div>
-            <img style={{maxHeight: 'calc(100vh - 100px)', maxWidth: 'calc(100vw - 1500px)'}} alt='Show' src={show._embedded.episodes[8].image.original} />
           </div>
         }
       </div>
     )
-    console.log(show._embedded.episodes.length);
   }
 }
 
